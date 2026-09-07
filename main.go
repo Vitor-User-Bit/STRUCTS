46a98982837b7b4641d0fbabd76d44cac81cc6b9
Vitor-User-Bit <cezervitor@gmail.com>
2026-09-06 21:58:41 -0300

    Ponteiro

--- main.go
+++ main.go
@@ -1,83 +1,28 @@
 package main
 
 import "fmt"
+
+func main() {
+	n1 := 10
+	n2 := n1
+	n3 := &n1
+	fmt.Println("\nPrimeiro número: ", n1)
+	fmt.Println("Copia do n1: ", n2)
+	fmt.Println("Ponteiro do primeiro número: ", n3)
+	fmt.Println("Ponteiro do primeiro número desreferenciado: ", *n3)
+
+	n1 = 12
+	fmt.Println("\nPrimeiro número: ", n1)
+	fmt.Println("Copia do n1: ", n2)
+	fmt.Println("Ponteiro do primeiro número: ", n3)
+	fmt.Println("Ponteiro do primeiro número desreferenciado: ", *n3)
+
+	*n3 = 21 //Alterando valor de n1 através do ponteiro
+	fmt.Println("\nPrimeiro número: ", n1)
+	fmt.Println("Copia do n1: ", n2)
+	fmt.Println("Ponteiro do primeiro número: ", n3)
+	fmt.Println("Ponteiro do primeiro número desreferenciado: ", *n3)
+}
+
 	
-type End struct {
-	Cidade string
-	Estado string
-}
-type Pessoa struct {
-	Nome string
-	Idade int
-	Profissao string
-	Endereco End
-}
-
-func main (){
-	pessoa1 := Pessoa{
-		Nome: "Vitor",
-		Idade:  30,
-		Profissao: "Programador",
-		Endereco: End{
-			Cidade: "São Paulo",
-			Estado: "SP",
-		},
-	}
-			fmt.Println("Struct completa: ") // PRINTANDO A STRUCT COMPLETA
-			fmt.Println(pessoa1,"\n")
-
-
-			//PEDINDO ESPECIFICAMENTE
-
-			fmt.Println("Nome: ", pessoa1.Nome)
-			fmt.Println("Nome: ", pessoa1.Endereco.Cidade)
-
-	pessoa1.Profissao = "Professor"
-
-			fmt.Println("Nova profissão: ", pessoa1.Profissao)
-
-	var pessoa2 Pessoa
-			fmt.Println(pessoa2)
-
-	pessoa2.Nome = "Maria"
-	pessoa2.Idade = 25
-	pessoa2.Profissao = "Designer"
-	pessoa2.Endereco.Cidade = "Brasilia"
-	pessoa2.Endereco.Estado = "DF"
-
-			fmt.Println("\nApós preencher os campos: ")
-			fmt.Println(pessoa2)
-
-	pessoas := []Pessoa{
-		pessoa1,
-		pessoa2,
-			{
-				Nome: "carlos",
-			Idade:  40,
-			Profissao: "Engenheiro",
-			Endereco: End{
-				Cidade: "São Paulo",
-				Estado: "SP",
-			},
-		},
-	}
-
-	for i, pessoa := range pessoas{
-
-		fmt.Println("Pessoa", i+1)
-
-		fmt.Println("Nome: ", pessoa.Nome)
-		fmt.Println("Idade: ", pessoa.Idade)
-		fmt.Println("Profissão: ", pessoa.Profissao)
-		fmt.Println("Cidade: ", pessoa.Endereco.Cidade)
-		fmt.Println("Estado: ", pessoa.Endereco.Estado)
-	}
-
-
-}
-
-			
-
-
-
 
